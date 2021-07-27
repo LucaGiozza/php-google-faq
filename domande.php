aa4a1db018ef7feadcebcefeb980568cca800890
Luca Giozza <luca.giozza@gmail.com>
2021-07-27 15:33:27 +0200

    header

---
+++ domande.php
@@ -0,0 +1,45 @@
+<!-- consegna -->
+<!-- Riscrivere questa pagina del sito google 
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione 
+di queste domande e risposte con PHP. -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- inizio header -->
+<header>
+    <img class="google" src="img/images.png" alt="">
+    <span class="privacy">Privacy e Termini</span>
+    <ul class="lista">
+        <li class="nav" > <strong>Introduzione</strong> </li>
+        <li class="nav"> <strong>Norme sulla privacy</strong> </li>
+        <li class="nav"><strong>Termini di servizio</strong> </li>
+        <li class="nav"><strong>Tecnologie</strong> </li>
+        <li class="dom"><strong>Domande frequenti</strong></li>
+    </ul>
+
+</header>
+
+<!-- inizio main -->
+<main>
+
+</main>
+
+<!-- inizio footer -->
+<footer>
+
+</footer>
+    
+</body>
+</html>
